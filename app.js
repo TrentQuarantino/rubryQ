00b448cd205a12280bb4f559439c91eb19b53a84
Mauro Bertozzi <teotozzi@gmail.com>
2014-11-02 16:26:23 +0100

    0.93 version

--- app.js
+++ app.js
@@ -5,20 +5,16 @@ $("#tabs").tabs();
 //Authentication with Dropbox
 var client = new Dropbox.Client({key:"iz1dsortzogoweu"});
 client.authenticate({interactive:false}, function(error){
-	if(error) {
-		alert('Authentication error:' + error);
-	}
+    if(error) {
+        alert('Authentication error:' + error);
+    }
 });
 if (client.isAuthenticated()) {
-	$("#login").hide();
-	$("#app").show();
+    $("#login").hide();
+    $("#app").show();
 }
-
 // Bind authenticate method to your login button and listen for click on button
-
 $("#login").on("click", client.authenticate());
-
-
 
 var datastoreManager = client.getDatastoreManager();
 datastoreManager.openDefaultDatastore(function (error, datastore) {
@@ -26,41 +22,42 @@ datastoreManager.openDefaultDatastore(function (error, datastore) {
         alert('Error opening default datastore: ' + error);
     }
 
-   // Let the user read all tasks by printing them to the screen
+    // Let the user read all tasks by printing them to the screen
     var taskTable = datastore.getTable('names');
     var results = taskTable.query({completed: false});
 
     for (var k=0; k<results.length;k++ ) {
         //$("#todos").append( "<li>"+results[k].get("taskname") + "</li>");
-
         var words = results[k].get("taskname");
         var iddio = words[0];
         console.log(iddio + "---" + words);
         $("#" +  iddio).append("<p id='paragraph'>"+words + "</p>");
     }
-	  $("li").addClass("list-group-item");
+    //$("li").addClass("list-group-item");
 
-
-  // Let users add tasks
-	  $("#add").on("click", function() {
+    // Let users add tasks
+    $("#add").on("click", function() {
         taskTable.insert({
-          taskname: $("#newTask").val(),
-        	completed: false,
-        	created: new Date()
-     	  });
+            taskname: $("#newTask").val(),
+            completed: false,
+            created: new Date()
+        });
     });
 
-  // As new tasks are added automatically update the task list
+    // As new tasks are added automatically update the task list
     datastore.recordsChanged.addListener(function (event) {
         var records = event.affectedRecordsForTable('names');
         for (var k=0; k<records.length;k++ ) {
-        	//$("#todos").append( "<li>"+records[k].get("taskname") + "</li>");
-            var words = records[k].get("taskname");
-            var iddio = words[0];
-            console.log(iddio + "---" + words);
-            $("#" +  iddio).append("<p id='paragraph'>"+words + "</p>");
+           //$("#todos").append( "<li>"+records[k].get("taskname") + "</li>");
+           var words = records[k].get("taskname");
+           var iddio = words[0];
+           console.log(iddio + "---" + words);
+           $("#" +  iddio).append("<p id='paragraph'>"+words + "</p>");
         }
         $("li").addClass("list-group-item");
     });
+    //$("li").addClass("list-group-item");
 });
 // No list visualization
+
+
